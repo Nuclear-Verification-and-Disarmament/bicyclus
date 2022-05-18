0695ec3de3e246276e9161845e235f1507b44d76
maxschalz <max.schalz@rwth-aachen.de>
2022-05-18 18:10:46 +0200

    Add example run-script

--- examples/README.md
+++ examples/README.md
@@ -1,8 +1,16 @@
 # Bicyclus minimal working example
 This MWE will show the basics on how Bicyclus can be used.
+We consider a very simple fuel cycle with a uranium source, an enrichment
+facility, and two repositories for enriched and depleted uranium, respectively.
+
+The goal in this scenario is to reconstruct the enrichment grade of the feed
+uranium (the feed assay) through measurement of the total depleted uranium mass.
+For simplicity, all other parameters are kept constant.
+
+## File overview
 The different files used are
-- `cyclus_input.json`: Cyclus input file, which could also be an `.xml` or `.py`
-  file)
+- `cyclus_input.json`: Base Cyclus input file.
+  This could also be an `.xml` or `.py` file.
 - `run.py`: Driver file, where the specific behaviour is defined (what to
   extract from each simulation, how to calculate likelihoods, ...)
 - `true_parameters.json`: The 'true' parameters, i.e., the ones used to generate
--- examples/run.py
+++ examples/run.py
@@ -1,10 +1,8 @@
-import util.log as log
+#!/usr/bin/env python3
 
-import argparse
+import json
+import math
 import os
-
-import cyclus
-
 from copy import deepcopy
 from collections import namedtuple
 
@@ -13,174 +11,304 @@ import numpy as np
 import pymc3 as pm
 import theano.tensor as tt
 
-import blackbox.blackbox as bb
-import blackbox.likelihood as lik
+import bicyclus.blackbox
+import bicyclus.util
+
 
 RANDOM_SEED = 12345
 
-# Layour of simulation parameters (mutate())
-SimulationParameters = namedtuple('SimulationParameters',
-                                  ('u235_frac', 'power_cap'))
-# Energy output in MWd
-SimulationOutput = namedtuple('SimulationOutput', ('produced_energy', ))
+# Using a namedtuple may be OP here but can improve clarity in more complex
+# scenarios.
+SimulationOutput = namedtuple('SimulationOutput', ('depleted_U_mass',))
 
 
-def samples_output_path(i, name='', dir='data', default=None):
-    task_id = log.task_identifier()
-    if default is None or default == '':
-        basepath = os.environ.get('HOME', None)
-        default = os.path.join(basepath, dir)
-    os.makedirs(default, exist_ok=True)
-    return os.path.join(default,
-                        'cyclus_trace_{}_{}_{}.cdf'.format(name, task_id, i))
+class SampleCyclus(bicyclus.blackbox.CyclusCliModel):
+    """This class is the main interface with Cyclus."""
+    def __init__(self, true_parameters, sampled_parameters):
+        self.true_parameters = true_parameters
+        self.sampled_parameters = sampled_parameters
+        self.current_parameters = None
 
+        with open("cyclus_input.json", "r") as f:
+            self.model = json.load(f)
 
-def cyclus_output_path(name='', default=''):
-    basepath = os.environ.get('TMP', None)
-    out_dir = os.environ.get('CYCLUS_OUT_DIR', '')
-    if basepath is None:
-        basepath = os.environ.get('WORK', None)
-    if basepath is None:
-        basepath = default
-    os.makedirs(os.path.join(basepath, out_dir), exist_ok=True)
-    return os.path.join(basepath, out_dir,
-                        name + '_' + log.task_identifier() + '.h5')
+        super().__init__()
 
+    def mutate(self, sample=None):
+        """Mutate the model, i.e. update its parameters.
 
-class SimpleSampleCyclus(bb.CyclusModel):
-    def __init__(self, *args, csk={}, **kwargs):
-        super(SimpleSampleCyclus, self).__init__(*args,
-                                                 **kwargs,
-                                                 cyclus_simstate_kwargs=csk)
+        Parameters
+        ----------
+        sample : list
+            Contains parameters corresponding to the parameters in
+            `sampled_parameters`. The order must be identical to the
+            alphabetically sorted order of keys in `sampled_parameters`.
+        """
+        # Set ground truth parameters.
+        if sample is None:
+            parameters = self.true_parameters
 
-    def mutate(self, params):
-        log.log_print("mutate:", params)
+        else:
+            # Alphabetical order. Only copy keys, as sampled_parameters
+            # contain the sampling range.
+            parameters = {k: None for k in self.sampled_parameters}
+            # Unpack alphabetically sorted model parameters.
+            for i, k in enumerate(sorted(self.sampled_parameters.keys())):
+                parameters[k] = sample[i]
+            bicyclus.util.log_print('Mutating: using parameters', parameters)
+
+        # Change the isotopics of the feed uranium.
         self.mut_model = deepcopy(self.model)
-
-        u235_frac = params[0]
-        u238_frac = 1 - u235_frac
-        powercap = params[1]
-
-        self.mut_model['simulation']['recipe'][1]['nuclide'][0]['comp'] = str(
-            u235_frac)
-        self.mut_model['simulation']['recipe'][1]['nuclide'][1]['comp'] = str(
-            u238_frac)
-
-        self.mut_model['simulation']['facility'][2]['config']['Reactor'][
-            'power_cap'] = str(powercap)
-
-        return
+        self.mut_model["simulation"]["recipe"][0]["nuclide"][0].update(
+            comp=parameters["feed_assay"])  # U235
+        self.mut_model["simulation"]["recipe"][0]["nuclide"][1].update(
+            comp=1.-parameters["feed_assay"])  # U238
+        self.current_parameters = parameters
 
     def result(self):
-        df = self.last_result.query("TimeSeriesPower")
-        log.log_print("Power:", df['Value'].sum())
-        return SimulationOutput(
-            produced_energy=df['Value'].sum())  #np.array(df['Value'].sum())
+        """Extract the results from the last simulation."""
+        target_facility_name = "DepletedUSink"
+        rval = bicyclus.cyclus_db.multi_agent_concs(
+            self.last_sqlite_file, [target_facility_name]
+        )  # Returns concentrations and masses.
+        total_mass = rval[1][target_facility_name]
+
+        # Using a namedtuple may be OP here but can improve clarity in more
+        # complex scenarios.
+        sim_output = SimulationOutput(depleted_U_mass=total_mass)
+        bicyclus.util.log_print("Simulation output:", sim_output)
+
+        return sim_output
 
 
-class SimpleNormalLikelihood(lik.NormalLikelihood):
-    def __init__(self, mu: SimulationOutput, sigma: SimulationOutput):
-        self.mu = mu
-        self.sigma = sigma
+class IsotopeLikelihood(bicyclus.blackbox.LikelihoodFunction):
+    """Class that calculates the likelihood."""
+    def __init__(self, truth: SimulationOutput, rel_sigma=0.5):
+        """Create a IsotopeLikelihood object.
 
-    def log_likelihood(self, so: SimulationOutput):
-        loglik = pm.Normal.dist(mu=self.mu.produced_energy,
-                                sigma=self.sigma.produced_energy).logp(
-                                    so.produced_energy).eval()
-        log.log_print(
-            "loglik:", loglik,
-            "for {} vs {} +/- {}".format(so.produced_energy,
-                                         self.mu.produced_energy,
-                                         self.sigma.produced_energy))
-        return loglik
+        Parameters
+        ----------
+        truth : SimulationOutput
+            The ground truth, i.e., simulation results using the true
+            parameters.
+        rel_sigma : float
+            Relative sigma *in percent*.
+        logdest : str (path) or None
+            Path where the log will be stored.
+        """
+        self.truth = truth
+        self.rel_sigma = rel_sigma
+
+    def log_structured_sample(self, simout: SimulationOutput,
+                              likelihood: float):
+        """Add results (concentrations, likelihood, parameters) to the log."""
+        if self.structured_log is not None:
+            d = {'likelihood': likelihood,
+                 'sink_masses': simout.additional_masses,
+                 'concentrations': {},
+                 'parameters': simout.parameters}
+            for sink in simout.composition.keys():
+                for iso, concentration in simout.composition[sink].items():
+                    if int(iso * 1e-4) in self.only_isos:
+                        if sink not in d["concentrations"].keys():
+                            d["concentrations"][sink] = {}
+                        d["concentrations"][sink][iso] = concentration
+
+            json.dump(d, self.structured_log)
+            print('', file=self.structured_log)
+            self.structured_log.flush()
+
+    def log_likelihood(self, output: SimulationOutput):
+        """Calculate the loglikelihood for a given measurement.
+
+        The calculation considers the depleted U mass in this case, however,
+        this can of course be generalised (taking into account, e.g., isotopic
+        compositions).
+        """
+        # Convert relative sigma to absolute sigma.
+        abs_sigma = lambda x: x * self.rel_sigma / 100.
+
+        # Normalise the differences. This is superfluous here but is important
+        # in other scenarios to make the different likelihood contributions
+        # comparable.
+        centered = output.depleted_U_mass - self.truth.depleted_U_mass
+        normalised = centered / abs_sigma(self.truth.depleted_U_mass)
+
+        # Define the standard normal distribution. It is recommended *not* to
+        # use PyMC's internal Normal distribution here, because it's evaluation
+        # is much more expensive than the one shown below.
+        std_normal = lambda x: math.exp(-x**2 / 2) / (2 * math.pi)**0.5
+        # TODO test if I have to insert a try-except clause here.
+        try:
+            llk = std_normal(normalised)
+            logllk = math.log(llk)
+        except ValueError as e:
+            if llk < 1e-30:  # Arbitrarily chosen very small value.
+                logllk = -np.inf
+            else:
+                raise e
+
+        bicyclus.util.log_print("Mass loglikelihood for depleted U: "
+                                f"{logllk:.5e}")
+
+        # Variable has to be returned as an array.
+        return np.array(logllk)
+
+
+def model(args):
+    """Set up the model, priors, etc."""
+    # Read prior distributions and groundtruths from files.
+    with open(args.sample_parameters_file, 'r') as f:
+        sample_parameters = json.load(f)
+    with open(args.true_parameters_file, 'r') as f:
+        true_parameters = json.load(f)
+
+    # Set up the Cyclus blackbox and obtain the groundtruth.
+    cyclus_model = SampleCyclus(true_parameters, sample_parameters)
+    groundtruth = cyclus_model.run_groundtruth()
+    bicyclus.util.log_print(f'Ground truth parameters are: {groundtruth}')
+
+    # Set up the likelihood operator.
+    loglikelihood_op = bicyclus.blackbox.CyclusLogLikelihood(
+        IsotopeLikelihood(groundtruth, rel_sigma=args.rel_sigma),
+        cyclus_model, memoize=True)
+
+    bicyclus.util.log_print('Building PyMC3 model.')
+    bicyclus.util.log_print('Sampling variables as follows:',
+                  [f'{k} => {v}' for (k, v) in sample_parameters.items()])
+    bicyclus.util.log_print('The true parameters are:',
+                  [f'{k} => {v}' for (k, v) in true_parameters.items()])
+
+    with pm.Model() as pymc_model:
+        # Transform the priors from the .json file to PyMC distributions.
+        pymc_priors = {
+            name: bicyclus.util.sampling_parameter_to_pymc(name, prior)
+            for name, prior in sample_parameters.items()}
+
+        bicyclus.util.log_print('Model variables:', pymc_priors)
+
+        # Add the likelihood to the model.
+        pm.Potential(
+            "observed",
+            loglikelihood_op(tt.as_tensor_variable(
+                [pymc_priors[k] for k in sorted(pymc_priors.keys())]
+            ))
+        )
+
+        # If you want reproducibility, you can set a seed and generate the
+        # initial values, as well (not done in this example).
+        start = None
+
+    return pymc_model, start
+
+
+def sample(args, pymc_model, start=None):
+    """Sample the random variables and generate the trace(s)."""
+    with pymc_model:
+        # Algorithm must be one of the methods defined by PyMC, see
+        # https://docs.pymc.io/en/v3/api/inference.html?highlight=step#step-methods.
+        if args.algorithm == "default":
+            algorithm = pm.Slice()
+        else:
+            try:
+                algorithm = pm.step_methods.__dict__[args.algorithm]
+            except KeyError:
+                msg = ("--algorithm must be one of the methods defined by "
+                       "PyMC, see https://docs.pymc.io/en/v3/api/inference.html?highlight=step#step-methods. "
+                       "Note that PyMC capitalises the first letter (e.g., "
+                       "'Metropolis' instead of 'metropolis').")
+                raise KeyError(msg)
+
+        # Set number of tuning steps.
+        if args.tuning_samples < 0:
+            args.tuning_samples = args.samples // 10
+
+        # Use chunk sampling and the 'standard' PyMC sampling algorithm.
+        if args.iter_sample <= 0:
+            bicyclus.util.log_print("Starting sampling.")
+            trace = None
+            for i in range(0, args.iterations):
+                bicyclus.util.log_print(
+                        f"sampling iteration {i} at {args.samples} samples "
+                        f"per iteration using {args.algorithm}, "
+                        f"initial parameters {start}")
+                # Refer to https://discourse.pymc.io/t/blackbox-likelihood-example-doesnt-work/5378
+                trace = pm.sample(
+                    args.samples,
+                    tune=args.tuning_samples,
+                    step=algorithm,
+                    chains=args.chains,
+                    cores=args.cores,
+                    start=start,
+                    return_inferencedata=False,
+                    compute_convergence_checks=False,
+                    trace=trace)
+                # This is https://github.com/pymc-devs/pymc-examples/issues/48#issue-839287296
+                # See https://arviz-devs.github.io/arviz/api/generated/arviz.from_pymc3.html#arviz-from-pymc3
+                save_trace(args, trace, i=i)
+
+        # Use pm.iter_sample. We found that this algorithm is much slower for
+        # unknown reasons.
+        else:
+            bicyclus.util.log_print(
+                f"Starting to sample iteratively (iter_sample: "
+                f"{args.iter_sample}), initial parameters: {start}")
+            if args.chains > 1:
+                bicyclus.util.log_print(
+                    "WARNING: --chains > 1, but sampling iteratively. This "
+                    "will not work -- sampling one chain only.")
+
+            sampler = pm.iter_sample(
+                args.samples,
+                algorithm,
+                start=start[0] if type(start) is list else start,
+                tune=args.tuning_samples)
+            sample_ix = 0
+            saved_traces = 0
+            # The sampling process starts here.
+            for trace in sampler:
+                sample_ix += 1
+                bicyclus.util.log_print(
+                    f'sampling: {saved_traces} {sample_ix}/{args.iter_sample}')
+                if sample_ix >= args.iter_sample:
+                    save_trace(args, trace, i=saved_traces)
+                    sample_ix = 0
+                    saved_traces += 1
+            save_trace(args, trace, i=saved_traces)
+
+    bicyclus.util.log_print("Sampling finished!")
+
+
+def save_trace(args, trace, i=0):
+    """Save the trace as .cdf file."""
+    task_id = bicyclus.util.task_identifier()
+
+    if args.output_path is None or args.output_path == '':
+        args.output_path = os.path.join(os.getcwd(), "data")
+    os.makedirs(args.output_path, exist_ok=True)
+
+    output_path = os.path.join(args.output_path,
+                               "cyclus_trace_{}_{}_{:03d}.cdf".format(
+                                   args.run, task_id, i))
+
+    bicyclus.util.log_print(f"Saving trace #{i} to file {output_path}.")
+    bicyclus.util.log_print(trace)
+    bicyclus.util.log_print(az.summary(az.from_pymc3(trace)))
+    location = az.from_pymc3(trace,
+                             density_dist_obs=False).to_netcdf(output_path)
+    bicyclus.util.log_print(f"Successfully saved trace #{i} to {location}")
 
 
 def main():
-    user = os.environ['USER']
+    """Main entry point of the script."""
+    parser = bicyclus.util.SamplingParser()
+    args = parser.get_args()
 
-    p = argparse.ArgumentParser(description='Cluster sampling test')
-    p.add_argument('--index', type=int, default=0, help='Instance index')
-    p.add_argument('--run', type=str, help='Name for this run.')
-    p.add_argument('--output-path',
-                   type=str,
-                   default=f'/work/{user}/data/',
-                   help='Output path for sample CDF files.')
-    p.add_argument('--log-path',
-                   type=str,
-                   default=f'/work/{user}/job_output/',
-                   help='log path.')
-    p.add_argument('--samples',
-                   type=int,
-                   default=400,
-                   help='Number of samples per chain per iteration')
-    p.add_argument(
-        '--iterations',
-        type=int,
-        default=5,
-        help=
-        'How many successive iterations with each --samples should be run. Total number of samples is `--samples * --iterations`'
-    )
-    p.add_argument('--algorithm',
-                   type=str,
-                   choices=('default', 'metropolis'),
-                   help='Which sampling algorithm we should use')
-    p.add_argument('--chains', type=int, default=1, help='Number of chains.')
-    p.add_argument("--debug", default=False, action="store_true",
-                   help="If set, print all output to STDOUT instead of storing"
-                        " it in a file. Overrides '--log-path' (if set).")
-    args = p.parse_args()
-    log.write_to_log_file(run=args.run, outpath=args.log_path,
-                          debug=args.debug)
+    bicyclus.util.write_to_log_file(run=args.run, outpath=args.log_path,
+                                    debug=args.debug)
 
-    ssc = SimpleSampleCyclus("sampling0.json",
-                             csk={
-                                 'output_path':
-                                 cyclus_output_path(args.run,
-                                                    default=args.output_path)
-                             })
-    ssc.run_groundtruth()
-    ssc.result()
-    likelihood = SimpleNormalLikelihood(
-        SimulationOutput(produced_energy=55366),
-        SimulationOutput(produced_energy=5e3))
-
-    loglikop = lik.CyclusLogLikelihood(likelihood, ssc)
-
-    with pm.Model() as model:
-        powercap = pm.Uniform("powercap", 1000, 1300)
-        u235frac = pm.Uniform("u235frac", 0.01, 0.1)
-
-        # This is "the inside out" - there is a better way, but that is
-        # slightly more complex to implement. Essentially, set
-        # observed=groundtruth, and extract powercap, u235frac from the pm
-        # model.
-        pm.DensityDist(
-            "observed",
-            lambda v: loglikop(v),
-            observed={'v': tt.as_tensor_variable([u235frac, powercap])})
-
-        trace = None
-        step = None
-        if args.algorithm == 'metropolis':
-            step = pm.Metropolis()
-
-        for i in range(0, args.iterations):
-            # Refer to https://discourse.pymc.io/t/blackbox-likelihood-example-doesnt-work/5378
-            trace = pm.sample(args.samples,
-                              tune=int(args.samples / 10),
-                              chains=args.chains,
-                              return_inferencedata=False,
-                              compute_convergence_checks=False,
-                              step=step,
-                              random_seed=RANDOM_SEED + args.index,
-                              trace=trace)
-            # this is https://github.com/pymc-devs/pymc-examples/issues/48#issue-839287296
-            # see https://arviz-devs.github.io/arviz/api/generated/arviz.from_pymc3.html#arviz-from-pymc3
-            #az.from_pymc3(trace, density_dist_obs=False).to_netcdf('/home/vf962887/data/precious_cyclus_trace_{}_{}.cdf'.format(log.task_identifier(), i))
-            az.from_pymc3(trace, density_dist_obs=False).to_netcdf(
-                samples_output_path(i, args.run, default=args.output_path))
-
-    log.log_print("Success")
+    pymc_model, start = model(args)
+    sample(args, pymc_model, start)
 
 
 if __name__ == "__main__":
