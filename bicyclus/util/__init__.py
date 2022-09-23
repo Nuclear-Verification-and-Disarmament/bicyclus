cda386a0cbfb0ae3599e2f998138892137cedcac
maxschalz <max.schalz@rwth-aachen.de>
2022-09-23 10:00:12 +0200

    Fix bicyclus.util.save_trace function
    This is part of #10 https://github.com/Nuclear-Verification-and-Disarmament/bicyclus/issues/10
    and fixes #9 https://github.com/Nuclear-Verification-and-Disarmament/bicyclus/issues/9.

--- bicyclus/util/__init__.py
+++ bicyclus/util/__init__.py
@@ -2,4 +2,4 @@ from .log import (log_file_path, log_init_debug_info, log_print,
     task_identifier, write_to_log_file)
 from .parsers import SamplingParser, CyclusRunParser
 from .postprocessing import extract_from_log, extract_single_log
-from .util import sampling_parameter_to_pymc
+from .util import sampling_parameter_to_pymc, save_trace
