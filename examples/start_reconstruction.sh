cd7ced5f2c0474e3e1fe0ffad2bc51d3134a0098
maxschalz <max.schalz@rwth-aachen.de>
2022-10-14 10:38:20 +0200

    Restructure examples/ directory

---
+++ examples/start_reconstruction.sh
@@ -0,0 +1,12 @@
+python3 run.py \
+  --run BicyclusExample \
+  --sample-parameters-file parameters/sample_parameters.json \
+  --true-parameters-file parameters/true_parameters.json \
+  --algorithm default \
+  --cores 4 \
+  --chains 4 \
+  --iterations 1 \
+  --samples 200 \
+  --tuning-samples 100 \
+  --rel-sigma 0.5 \
+  --debug  # Print log to CLI, don't store log in a separate file.
