5e186f9f8d144bfc851e1834b601eb271a6d1a16
maxschalz <max.schalz@rwth-aachen.de>
2022-12-22 11:40:37 +0100

    Update cyclus_db.__init__
    This allows to import the `run_with_conn` function into a driver script.

--- bicyclus/cyclus_db/__init__.py
+++ bicyclus/cyclus_db/__init__.py
@@ -2,6 +2,12 @@
 
 This module contains code for extracting results from Cyclus' simulation output databases.
 """
-from .extract import (extract_isotope_concentrations, extract_mass,
-    extract_transaction_composition, get_all_agents, multi_agent_concs,
-    multi_agent_transactions)
+from .extract import (
+    extract_isotope_concentrations,
+    extract_mass,
+    extract_transaction_composition,
+    get_all_agents,
+    multi_agent_concs,
+    multi_agent_transactions,
+    run_with_conn
+)
